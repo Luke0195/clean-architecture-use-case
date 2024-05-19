fdab526ae86c85ee2d0c89a8965fa6cff3d11104
Lucas Santos <lucas201273@gmail.com>
2024-05-18 22:48:25 -0300

    refactor: move error to a helper method

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,19 +1,14 @@
 import { type HttpRequest, type HttpResponse } from '../protocols/http'
 import { MissingParamError } from '../errors/missing-param-error'
+import { badRequest } from '../helpers/http-helper'
 export class SignUpController {
   handle(httRequest: HttpRequest): HttpResponse {
     if (!httRequest.body.name) {
-      return {
-        statusCode: 400,
-        body: new MissingParamError('Missing Param: name')
-      }
+      return badRequest(new MissingParamError('Missing Param: name'))
     }
 
     if (!httRequest.body.email) {
-      return {
-        statusCode: 400,
-        body: new MissingParamError('Missing Param: email')
-      }
+      return badRequest(new MissingParamError('Missing Param: email'))
     }
 
     return { statusCode: 201, body: {} }
