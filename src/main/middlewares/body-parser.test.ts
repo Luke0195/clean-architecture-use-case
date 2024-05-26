0a36e63cb8d1559ee15058a592e3a3f2b215e32a
Lucas Santos <lucas201273@gmail.com>
2024-05-26 09:24:49 -0300

    feat: ensure AppServer implements cors when is enabled

--- src/main/middlewares/body-parser.test.ts
+++ src/main/middlewares/body-parser.test.ts
@@ -1,11 +1,12 @@
 import request from 'supertest'
 import app from '../config/app'
 
-describe('BodyParser Middleware', () => {
-  test('', async () => {
+describe('Body Parser Middleware', () => {
+  test('Should parser body as json', async () => {
     app.post('/test_body_parser', (request, response) => {
       response.send(request.body)
     })
+
     await request(app)
       .post('/test_body_parser')
       .send({ name: 'Lucas' })
